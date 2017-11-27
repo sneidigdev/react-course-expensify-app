c59f12ba16d68b3ef60ed771ad550fe7d4b71653
sneidig <sneidigdev@gmail.com>
2017-11-27 14:21:01 -0500

    Changes for deploying, building, and run on heroku

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Server started');
 });
